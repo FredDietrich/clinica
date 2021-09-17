a52e457792999dfdb5855b2b8f75edb8974bc25e
Frederico Dietrich <frederico.machry@gmail.com>
2021-09-16 21:33:00 -0300

    Arrumando

--- clinica.py
+++ clinica.py
@@ -1,23 +1,3 @@
 #!/usr/bin/env python3
-import getpass
-import login, cadastro
-print("""
-**********************************************************************
-**                                                                  **
-**      BEM VINDO AO SISTEMA DE CLINICA MEDICA POR TECH CONNECT     **
-**                                                                  **
-**********************************************************************
-""")
-print("""
-OPCOES:
-(1) LOGIN
-(2) CADASTRO
-(3) SAIR
-""")
-escolhaLogin = int(input('Insira a opcao:'))
-if(escolhaLogin == 1):
-    login.main()
-elif(escolhaLogin == 2):
-    cadastro.main()
-elif(escolhaLogin == 3):
-    exit()
+
+
